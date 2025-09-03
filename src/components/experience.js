5498ac3441eb0abfc7d0ec29d57d5966ebdaeca5
Sahil Gokhale <sahil.gokhale2279@gmail.com>
2025-09-02 22:21:11 -0400

    Trigger GitHub Pages rebuild

--- src/components/experience.js
+++ src/components/experience.js
@@ -24,7 +24,7 @@ function Experience() {
                 <img src="/University of waterloo logo.png" alt="University of Waterloo Logo" style={{ width: 90, height: 'auto', borderRadius: '10px', background: '#f6f8fa', boxShadow: '0 1px 6px rgba(25, 118, 210, 0.08)' }} />
                 <div style={{ textAlign: 'left' }}>
                     <h4 style={{ margin: 0, fontSize: '1.3rem', fontWeight: 700, color: '#22223b' }}>Pallet Connect - We Accelerate</h4>
-                    <p style={{ margin: '0.7rem 0 0.2rem 0', fontSize: '1.08rem' }}>Procuring real world applicable solutions to various environmental and societal issues through data analysis.</p>
+                    <p style={{ margin: '0.7rem 0 0.2rem 0', fontSize: '1.08rem' }}>Procuring real world applicable solutions to various environmental issues through data analysis.</p>
                     <p style={{ margin: 0, fontSize: '1.08rem' }}>Utilize PowerBi, Microsoft Excel, and PowerQuery to manage and analyze various large scale data sets.</p>
                 </div>
             </div>
